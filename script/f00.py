e05883bca70143e6b63ba1011bd02fa1e9a5f5a0
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2014-10-07 22:14:51 -0400

    Update to datac 0.3.0

--- script/f00.py
+++ script/f00.py
@@ -9,6 +9,15 @@ import matplotlib.pyplot as plt
 import copy
 import os
 
+# Overload the `Datac.plot` method to give me what I want
+class f00(datac.Datac):
+    def plot(self):
+        fig = plt.figure(figsize=(5.,4.))
+        plt.plot(self.abscissae, self.ordinates)
+        plt.xlabel("Bandgap [eV]")
+        plt.ylabel("Efficiency")
+
+
 script_abspath = os.path.dirname(os.path.abspath(__file__))
 script_filename = os.path.basename(os.path.abspath(__file__))
 script_filename_root = os.path.splitext(script_filename)[0]
@@ -18,13 +27,18 @@ data_filename = script_filename_root + ".dat"
 data_dir_relpath = "dat"
 datafile_fqpn = os.path.join(parent_repo_abspath, data_dir_relpath, data_filename)
 
+fig_filename = script_filename_root + ".pdf"
+build_dir_relpath = "build"
+figfile_fqpn = os.path.join(parent_repo_abspath, build_dir_relpath, fig_filename)
+
 
 # Set up abscissae.
 params = {"temp_sun": 6000.}
 bandgaps = np.linspace(0, 3.25, 100)
 
-abscissae = datac.generate_abscissae(bandgaps, "bandgap", params)
-data = datac.generate_ordinates(abscissae, ibei.SQSolarcell, "calc_efficiency")
+dt = f00(params, bandgaps, "bandgap", ibei.SQSolarcell.calc_efficiency)
+dt.savefig(figfile_fqpn, bbox_inches=0)
+# dt.show()
 
 # Write data to file.
-datac.write_json(datafile_fqpn, data)
+# datac.write_json(datafile_fqpn, data)
