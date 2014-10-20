8bad72ecdade260917517fec28b071cefb7c48b6
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2014-10-20 10:36:19 -0400

    Add code to calc max efficiency and plot eta vs V
    I included dependence on the `datac` module, basically copying the
    functionality of f00.py

--- script/d01.py
+++ script/d01.py
@@ -3,9 +3,26 @@
 Maximum efficiency of single junction solar cell according to DeVos
 """
 import ibei
+import datac
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Overload the `Datac.plot` method to give me what I want
+class f01(datac.Datac):
+    def plot(self):
+        # fig = plt.figure(figsize=(5.,4.))
+        plt.plot(self.abscissae, self.ordinates)
+        plt.xlabel("Voltage [V]")
+        plt.ylabel("Efficiency")
+
+
 params = {"temp_sun": 5762.,
           "temp_planet": 289.,
           "bandgap": 1.15,}
 
-sc = ibei.DeVosSolarcell(params)
-print sc.calc_efficiency()
+voltages = np.linspace(0, 0.975 * params["bandgap"], 1000)
+
+dt = f01(params, voltages, "voltage", ibei.DeVosSolarcell.calc_efficiency)
+# dt.show()
+print max(dt.ordinates)
