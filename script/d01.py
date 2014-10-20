f53999f396ce3821993e0e0859b4a873f9d67f3f
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2014-10-20 10:38:20 -0400

    Remove superfluous code; calc only efficiency

--- script/d01.py
+++ script/d01.py
@@ -8,21 +8,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# Overload the `Datac.plot` method to give me what I want
-class f01(datac.Datac):
-    def plot(self):
-        # fig = plt.figure(figsize=(5.,4.))
-        plt.plot(self.abscissae, self.ordinates)
-        plt.xlabel("Voltage [V]")
-        plt.ylabel("Efficiency")
-
-
 params = {"temp_sun": 5762.,
           "temp_planet": 289.,
           "bandgap": 1.15,}
 
 voltages = np.linspace(0, 0.975 * params["bandgap"], 1000)
 
-dt = f01(params, voltages, "voltage", ibei.DeVosSolarcell.calc_efficiency)
-# dt.show()
+dt = datac.Datac(params, voltages, "voltage", ibei.DeVosSolarcell.calc_efficiency)
 print max(dt.ordinates)
